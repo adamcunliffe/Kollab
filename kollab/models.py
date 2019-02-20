ab3f26b2937fd8148c1245cfd498d7a1b34c9dd5
briancollins-92 <bcollins.sw@gmail.com>
2019-02-20 18:38:09 +0000

    population script added

--- kollab/models.py
+++ kollab/models.py
@@ -6,23 +6,37 @@ kollab/models.py
 
 How to use django.models : https://docs.djangoproject.com/en/2.1/ref/models/fields/
 
-
+classes to be added:
+	chat history
+	API information which supports us using facebook, soundcloud apis etc
+	
 
 
 '''
 
 class Tag(models.Model):
+	# Might need to make each tage unique to ensure association between people via tags
+	
 	name = models.CharField(max_length=128)
 	
 	def __str__(self):
 		return self.name
 
 class User(models.Model):
+
 	name = models.CharField(max_length=128)
 	
-	# Users chosen location stored as latitude and logditude 
+	# Users chosen location stored as latitude and longditude 
+	# floatfield is just a decimal number like 50.938892 or -14.324333
 	lat = models.FloatField()
 	lon = models.FloatField()
+	
+	''' to be added:
+		portfolio --> one to one / one to many? --> portfolio class
+		firstname and secondname
+		search settings --> one to one --> search settings class
+		
+	'''
 	
 	# User has a many-to-many relationship with Tags becuase
 	# tags are shared between users, (eg, musician tag can belong to two 
@@ -37,6 +51,12 @@ class User(models.Model):
 class Project(models.Model):
 	name = models.CharField(max_length=128)
 	
+	''' to be added:
+		privacy setting - could be simple private/public or one to one link to a 
+			project settings class
+		projectAdmin --> one to many -->Admin
+	'''
+	
 	# many to many for same reason as with person
 	tags = models.ManyToManyField(Tag)
 	
@@ -49,10 +69,13 @@ class Project(models.Model):
 class Membership(models.Model):
 	# this sets a user as a member and ensures that if
 	# the user is deleted from the project that they are not deleted 
-	# from the database! blank = true ensures that when creating a new projet
-	# that having a user is not a mandatory field
+	# from the database! blank = true ensures that when creating a new membership
+	# that having a user/project is not a mandatory field
 	user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
 	project = models.ForeignKey(Project,models.SET_NULL, blank=True, null=True)
 	
+	''' to be added:
+		date started membership / ended membership		
+	'''
 	def __str__(self):
 		return self.user + ' ' + self.project
