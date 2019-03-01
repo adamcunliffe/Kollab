896b2983ffd3b4f80a6826b791e4cb1a4c0624bc
briancollins-92 <bcollins.sw@gmail.com>
2019-03-01 16:43:04 +0000

    Modified directory to host html files

--- kollab/views.py
+++ kollab/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 	
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return render(request, 'kollab/index.html')
+	
+def login(request):
+	return render(request, 'kollab/login.html')
