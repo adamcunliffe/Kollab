86284b056f45aed75eae846a3a246fde977cbeb4
briancollins-92 <bcollins.sw@gmail.com>
2019-03-07 18:40:07 +0000

    forms .py added

---
+++ kollab/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.models import User
+from kollab.models import UserProfile
+'''
+class UserForm(forms.ModelForm):
+	firstName = forms.CharField(max_length=128)
+	secondName = forms.CharField(max_length=128)
+'''
