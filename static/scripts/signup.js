1de9dbdba55549c617b14a735fbc0b21fe929783
adamcunliffe <43607023+adamcunliffe@users.noreply.github.com>
2019-03-21 17:48:07 +0000

    Add files via upload

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -53,6 +53,19 @@ function postData(formData){
 	});
 }
 
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Location found."
+}
 
 
 
